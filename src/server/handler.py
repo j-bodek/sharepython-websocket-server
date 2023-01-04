8444d7df6c048512ece140f32b112c08345b8b61
Liljack118 <kamilzajaczek4@gmail.com>
2023-01-04 19:29:42 +0100

    send code updates to multiple websockets

--- src/server/handler.py
+++ src/server/handler.py
@@ -3,6 +3,8 @@ from urllib import parse
 import logging
 import json
 from .redis import REDIS
+import websockets
+import secrets
 
 
 class WebSocketHandler(object):
@@ -18,30 +20,45 @@ class WebSocketHandler(object):
     redis = REDIS
 
     def __init__(self):
-        self.websocket = None
+        self.websockets = set()
         self.codespace_uuid = None
 
     async def __call__(self, websocket):
-        self.websocket = websocket
-        token = parse.parse_qs(parse.urlparse(self.websocket.path).query).get(
+        # self.websocket = websocket
+
+        token = parse.parse_qs(parse.urlparse(websocket.path).query).get(
             "token", [None]
         )[0]
         # if request not authenticated connection will be closed
-        await self.authenticate(token)
+        await self.authenticate(token, websocket)
 
         # This will be iterating over messages received on
         # the connection until the client disconnects
-        async for message in self.websocket:
-            await self.dispatch(message)
+        try:
+            self.websockets.add(websocket)
+            # send connected message
+            await websocket.send(
+                json.dumps(
+                    {
+                        "operation": "connected",
+                        "data": {"id": secrets.token_urlsafe(12)},
+                    }
+                )
+            )
+            # listen client messages
+            async for message in websocket:
+                await self.dispatch(message, websocket)
+        finally:
+            self.websockets.remove(websocket)
 
-    async def authenticate(self, token: str) -> None:
+    async def authenticate(self, token: str, websocket) -> None:
         """
         Authenticate request by given token and if token is valid
         update codespace_uuid
         """
 
         if not token:
-            await self.websocket.close(1011, "Missing token")
+            await websocket.close(1011, "Missing token")
             return
 
         async with aiohttp.ClientSession() as session:
@@ -49,15 +66,16 @@ class WebSocketHandler(object):
                 f"{self.api_base_url}codespace/{token}/?fields=uuid"
             ) as resp:
                 if resp.status != 200:
-                    await self.websocket.close(1011, "Invalid token")
+                    await websocket.close(1011, "Invalid token")
                     return
 
                 data = await resp.json()
                 self.codespace_uuid = data["uuid"]
 
-    async def dispatch(self, message):
+    async def dispatch(self, message, websocket):
         # Try to dispatch to the right operation; if a operation doesn't exist
         # close websocket connection
+
         message = json.loads(message)
         if (
             operation := message.get("operation")
@@ -66,9 +84,9 @@ class WebSocketHandler(object):
         else:
             handler = self.operation_not_allowed
 
-        await handler(message)
+        await handler(message, websocket)
 
-    async def operation_not_allowed(self, message):
+    async def operation_not_allowed(self, message, websocket):
         """
         Close websocket connection and return proper reason
         """
@@ -77,15 +95,18 @@ class WebSocketHandler(object):
             f"{message.get('operation')} Operation Is Not Allowed",
         )
 
-        await self.websocket.close(
+        await websocket.close(
             1011, f"'{message.get('operation')}' operation is not allowed"
         )
 
-    async def insert_value(self, message) -> None:
+    async def insert_value(self, message, websocket) -> None:
         if data := self.redis.hgetall(self.codespace_uuid):
             data["code"] = (
                 data["code"][: message["input"]["position"]["start"]]
                 + message["input"]["value"]
                 + data["code"][message["input"]["position"]["end"] :]
             )
+
             self.redis.hmset(self.codespace_uuid, data)
+
+            websockets.broadcast(self.websockets, json.dumps(message))
