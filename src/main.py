fbd407e4195d9c04160fb9292cacc1209a5c25e5
Liljack118 <kamilzajaczek4@gmail.com>
2023-01-05 18:27:44 +0100

    improve speed, and readibility with sanic

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-import asyncio
-import websockets
+from sanic import Sanic, Request, Websocket
 from server.handler import WebSocketHandler
+from server.authentication import Authenticate
 
+app = Sanic(name="WebSocketServer")
 
 handler = WebSocketHandler()
+authenticate = Authenticate()
 
 
-async def main():
-    async with websockets.serve(handler, "", 8888):
-        await asyncio.Future()  # run forever
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+@app.websocket("/<token:str>/")
+async def feed(request: Request, ws: Websocket, token: str):
+    uuid = await authenticate(ws, token)
+    await handler(ws, uuid)
--- src/server/handler.py
+++ src/server/handler.py
@@ -1,17 +1,12 @@
-import aiohttp
-from urllib import parse
 import logging
 import json
 from .redis import REDIS
-import websockets
 import secrets
+from server.channel import WebSocketChannels
 
 
 class WebSocketHandler(object):
 
-    # big thx https://stackoverflow.com/a/56297455/14579046
-    # host should be name of container instead of localhost
-    api_base_url = "http://api:8000/api/"
     operation_names = [
         "insert_value",
         "remove_value",
@@ -20,22 +15,15 @@ class WebSocketHandler(object):
     redis = REDIS
 
     def __init__(self):
-        self.websockets = set()
-        self.codespace_uuid = None
+        self.channels = WebSocketChannels()
+        # self.codespace_uuid = None
 
-    async def __call__(self, websocket):
-        # self.websocket = websocket
-
-        token = parse.parse_qs(parse.urlparse(websocket.path).query).get(
-            "token", [None]
-        )[0]
-        # if request not authenticated connection will be closed
-        await self.authenticate(token, websocket)
+    async def __call__(self, websocket, codespace_uuid):
 
         # This will be iterating over messages received on
         # the connection until the client disconnects
         try:
-            self.websockets.add(websocket)
+            self.channels.add_client(codespace_uuid, websocket)
             # send connected message
             await websocket.send(
                 json.dumps(
@@ -47,32 +35,11 @@ class WebSocketHandler(object):
             )
             # listen client messages
             async for message in websocket:
-                await self.dispatch(message, websocket)
+                await self.dispatch(message, codespace_uuid, websocket)
         finally:
-            self.websockets.remove(websocket)
+            self.channels.remove_client(codespace_uuid, websocket)
 
-    async def authenticate(self, token: str, websocket) -> None:
-        """
-        Authenticate request by given token and if token is valid
-        update codespace_uuid
-        """
-
-        if not token:
-            await websocket.close(1011, "Missing token")
-            return
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                f"{self.api_base_url}codespace/{token}/?fields=uuid"
-            ) as resp:
-                if resp.status != 200:
-                    await websocket.close(1011, "Invalid token")
-                    return
-
-                data = await resp.json()
-                self.codespace_uuid = data["uuid"]
-
-    async def dispatch(self, message, websocket):
+    async def dispatch(self, message, codespace_uuid, websocket):
         # Try to dispatch to the right operation; if a operation doesn't exist
         # close websocket connection
 
@@ -84,9 +51,9 @@ class WebSocketHandler(object):
         else:
             handler = self.operation_not_allowed
 
-        await handler(message, websocket)
+        await handler(message, codespace_uuid, websocket)
 
-    async def operation_not_allowed(self, message, websocket):
+    async def operation_not_allowed(self, message, codespace_uuid, websocket):
         """
         Close websocket connection and return proper reason
         """
@@ -99,14 +66,18 @@ class WebSocketHandler(object):
             1011, f"'{message.get('operation')}' operation is not allowed"
         )
 
-    async def insert_value(self, message, websocket) -> None:
-        if data := self.redis.hgetall(self.codespace_uuid):
+    async def insert_value(self, message, codespace_uuid, websocket) -> None:
+        if data := self.redis.hgetall(codespace_uuid):
             data["code"] = (
                 data["code"][: message["input"]["position"]["start"]]
                 + message["input"]["value"]
                 + data["code"][message["input"]["position"]["end"] :]
             )
 
-            self.redis.hmset(self.codespace_uuid, data)
+            self.redis.hmset(codespace_uuid, data)
 
-            websockets.broadcast(self.websockets, json.dumps(message))
+            await self.broadcast(codespace_uuid, json.dumps(message))
+
+    async def broadcast(self, codespace_uuid, msg):
+        for client in self.channels.get_clients(codespace_uuid):
+            await client.send(msg)
