2e00551461318addb48de551a409d192694712af
Liljack118 <kamilzajaczek4@gmail.com>
2023-01-05 18:22:39 +0100

    move authentication to separate class

---
+++ src/server/authentication.py
@@ -0,0 +1,29 @@
+import aiohttp
+
+
+class Authenticate(object):
+
+    # big thx https://stackoverflow.com/a/56297455/14579046
+    # host should be name of container instead of localhost
+    api_base_url = "http://api:8000/api/"
+
+    async def __call__(self, websocket, token: str) -> None:
+        """
+        Authenticate request by given token and if token is valid
+        update codespace_uuid
+        """
+
+        if not token:
+            await websocket.close(1011, "Missing token")
+            return
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"{self.api_base_url}codespace/{token}/?fields=uuid"
+            ) as resp:
+                if resp.status != 200:
+                    await websocket.close(1011, "Invalid token")
+                    return
+
+                data = await resp.json()
+                return data["uuid"]
