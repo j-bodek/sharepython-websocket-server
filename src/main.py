2f6871c152680476feb58297d6d09557673248b8
Liljack118 <kamilzajaczek4@gmail.com>
2023-01-10 18:41:23 +0100

    run sanic app on port specified in env variables

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from sanic import Sanic, Request, Websocket
 from server.handlers.connection_handler import connection_handler
 from typing import Type
+import os
 
 app = Sanic(name="WebSocketServer")
 
@@ -8,3 +9,7 @@ app = Sanic(name="WebSocketServer")
 @app.websocket("/<token:str>/")
 async def codespace(request: Type[Request], ws: Type[Websocket], token: str) -> None:
     await connection_handler(ws, token, request.app)
+
+
+if __name__ == "__main__":
+    app.run(port=os.environ.get("PORT"))
